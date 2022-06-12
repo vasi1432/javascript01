75142f5b715b5054cd7d5224d4188060f5cf5c0c
vasi1432 <vasiahmed1432@gmail.com>
2022-06-12 10:44:33 +0530

    added onemore question

--- 21--String reverse with reversing of individual words/assets/script.js
+++ 21--String reverse with reversing of individual words/assets/script.js
@@ -1,4 +1,14 @@
 const str = "iruog demha isav";
+//reverse using inbuild method
+str01 = [...str];
+res = str01.reverse().join("");
+console.log(res);
+// .
+// .
+// .
+// .
+// .
+//reverse using hardcode
 const result = [];
 for (let i = str.length - 1; i >= 0; i--) {
   result.push(str[i]);
