56a5ae81ea8a79102521e6f702f76fd747b8d747
vasi1432 <vasiahmed1432@gmail.com>
2022-07-02 18:41:31 +0530

    added 53rd question

---
+++ 53--To check whether particular word/assets/script.js
@@ -0,0 +1,10 @@
+// To check whether particular word/number present in sentence or not using inbuilt function
+const sentense = "hii my name is vasi";
+const check = "vasi";
+const result = sentense.includes(check);
+console.log(result);
+
+const number = [1, 2, 3, 4, 5, 6];
+const numCheck = 5;
+const result01 = number.includes(numCheck);
+console.log(result01);
