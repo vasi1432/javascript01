a6d1d473baa2320810bd5ac7b43c6072a6d19c64
vasi1432 <vasiahmed1432@gmail.com>
2022-07-12 18:07:34 +0530

    added new question

---
+++ 71--To find kth smallest or largest element in an array/assets/script.js
@@ -0,0 +1,12 @@
+const input = [2, 1, 4, 3, 6, 5, 9];
+
+//1
+let arr = input.sort();
+let length = arr.length;
+
+let result = {
+  smallest: arr[0],
+  largest: arr[length - 1],
+};
+console.log(result);
+//2
