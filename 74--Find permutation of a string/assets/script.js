326ccde0cc774f4bbf80caff80b1b5447905a100
vasi1432 <vasiahmed1432@gmail.com>
2022-07-15 17:05:46 +0530

    added new question

--- 74--Find permutation of a string/assets/script.js
+++ 74--Find permutation of a string/assets/script.js
@@ -2,14 +2,11 @@ const input = "let";
 let result = [];
 for (let i = 0; i < input.length; i++) {
   result.push(input[i]);
-  for (let j = 0; j < input.length; j++) {
-    if (input[i] == input[j]) {
-      j++;
-      result.push(input[1] + input[2]);
-      break;
-    }
+
+  if (input[1] !== input[2]) {
   }
 }
+
 console.log(result);
 //yet to complete
 // Output
---
+++ 76--To find the power of x with or without using Math function/assets/script.js
@@ -0,0 +1,12 @@
+// 1
+function findPowOfNumber(number, exponent) {
+  return number ** exponent;
+}
+const input = findPowOfNumber(2, 3);
+console.log(input);
+//2
+function findPowOfNumber01(number, exponent) {
+  return Math.pow(number, exponent);
+}
+const input01 = findPowOfNumber01(2, 3);
+console.log(input01);
