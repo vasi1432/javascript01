a5f87e1f6c901550f0ed34c66b9a546aa86031c9
vasi1432 <vasiahmed1432@gmail.com>
2022-07-21 16:35:58 +0530

    added one more approach in 80th question

--- 80--Move all the zeroes at the end of an elements/assets/script.js
+++ 80--Move all the zeroes at the end of an elements/assets/script.js
@@ -1,3 +1,4 @@
+//1
 function pushZero(arr) {
   let result = [];
   let temp = [];
@@ -11,8 +12,21 @@ function pushZero(arr) {
 
   return (result = result.concat(temp));
 }
-const input = pushZero([1, 0, 2, 0, 0, 9, 0, 6, 7]);
+const input = pushZero("approach 1", [1, 0, 2, 0, 0, 9, 0, 6, 7]);
 console.log(input);
 
 // Output
 // [1, 2, 9, 6, 7, 0, 0, 0, 0];
+//.
+//2
+var moveZeroes = function (nums) {
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] === 0) {
+      let temp = nums.splice(i, 1);
+      nums.push(temp);
+      i--;
+    }
+  }
+  return nums;
+};
+console.log(moveZeroes([1, 2, 9, 6, 7, 0, 0, 0, 0]));
