47d2dcdb83beb528901c2bcb26e96709dee7f5c5
vasi1432 <vasiahmed1432@gmail.com>
2022-07-21 16:30:01 +0530

    added new question

--- 81--Create a function to calculate the sum of all the numbers in a jagged array/assets/script.js
+++ 81--Create a function to calculate the sum of all the numbers in a jagged array/assets/script.js
@@ -1,7 +1,19 @@
 const input = [1, 2, [3, [4], [5, [2, 23], 6]], [7, 10]];
-
+//1
 let arr = input.flat(3);
-console.log(arr);
 
 let result = arr.reduce((a, b) => a + b);
-console.log(result);
+console.log("approach 1", result);
+
+//2
+function jaggedArraySum(arr) {
+  let result = 0;
+  for (element of arr) {
+    if (Array.isArray(element)) {
+      element = jaggedArraySum(element);
+    }
+    result += element;
+  }
+  return result;
+}
+console.log(jaggedArraySum([1, 2, [3, [4], [5, [2, 23], 6]], [7, 10]])); //28
