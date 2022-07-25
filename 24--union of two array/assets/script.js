a4e162d6aa48e8059b46434306972ddbb03a23fe
vasi1432 <vasiahmed1432@gmail.com>
2022-07-25 16:15:31 +0530

    added new approach in 24th question

--- 24--union of two array/assets/script.js
+++ 24--union of two array/assets/script.js
@@ -1,20 +1,10 @@
-const arr01 = [2, 3, 4, 34, 445, 656, 5, 32, 2445, 656, 3, 42, 1, 2];
-const arr02 = [3, 4, 1, 634, 5, 32, 2445, 656, 3, 42, 1, 2];
-const result = [];
-const result01 = [];
-
-for (let i = 0; i < arr01.length; i++) {
-  for (let j = 0; j < arr02.length; j++) {
-    if (arr01[i] === arr02[j]) {
-      result.push(arr02[j]);
+function Union(arr1, arr2) {
+  for (let j = 0; j < arr2.length; j++) {
+    if (arr1.includes(arr2[j])) {
+    } else {
+      arr1.push(arr2[j]);
     }
   }
+  return arr1;
 }
-console.log(result.sort());
-
-for (let i = 0; i < result.length; i++) {
-  if (result[i] !== result[i + 1]) {
-    result01.push(result[i]);
-  }
-}
-console.log(result01);
+console.log(Union([1, 2, 3], [3, 4, 5]));
